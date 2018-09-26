ce08383543574bd3dfe56f76945ad79347481ca3
phyraytor <phyraytor@gmail.com>
2018-09-26 13:50:21 +0300

    priest full skills, bat not all skills works

--- priest.py
+++ priest.py
@@ -4,104 +4,74 @@ class Mag(person.Person):
 
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.max_hitpoint = 1000
+        self.max_hitpoint = 1200
         self.stats['defense'] = 500
-        self.stats['resistance'] = 1500
-        self.stats['damage'] = 500
+        self.stats['resistance'] = 1600
+        self.stats['damage'] = 400
         self.stats['hitpoint'] = self.max_hitpoint
-        self.stats['chance_krit'] = 79
+        self.stats['chance_krit'] = 49
         self.stats['precision'] = 10
         self.stats['evasion'] = 20
 
-        '''-------------------------------------***********-------------------------------------'''
-        '''--------------**********-------------Magic Arrow--------------**********-------------'''
-        '''-------------------------------------***********-------------------------------------'''
+        '''----------------------------------------******---------------------------------------'''
+        '''--------------**********----------------Prayer----------------**********-------------'''
+        '''----------------------------------------******---------------------------------------'''
 
         def finish(name: str, damage: float) -> dict:
-            '''def kill_shield(damage: float) -> dict:
-                result = {'target': {}}
-                result['target']['hit'] = {
-                    'name': 'frost shiled',
-                    'type': 'magic',
-                    'damage': damage
-                }
-                return result
-
-            def end_shield(hil: float) -> dict:
-                result = {'self': {}}
-                result['self']['hil'] = hil
-                return result
-            '''
+            
             result = {'target': {}, 'self': []}
-
-            '''
-            result['self'].append({
-                'func': self.create_shield,
-                'name': 'frost shiled',
-                'hitpoint': self.stats['damage'],
-                'resist': 20,
-                'time': 10,
-                'die': kill_shield,
-                'end': end_shield,
-            })
 
             result['self'].append({
                 'func': self.get_hitpoint,
                 'name': 'my hill',
-                'damage': damage * 0.1,
-            })'''
-
-            result['self'].append({
-                'func': self.create_bonus,
-                'name': 'Insight',
-                'time': 5,
-                'mult_damage': 0.02,
+                'damage': damage,
             })
 
-            result['target']['hit'] = {
+            '''result['target']['hit'] = {
                 'name': name,
                 'type': 'magic',
                 'damage': damage,
-            }
-
-            '''result['target']['curse'] = {
-                'name': 'minus defense',
-                'time': 3,
-                'defense': 100,
-            }'''
+            }''' #Для теста
 
             return result
 
-        magic_arrow = person.classSkill.Skill(name='Magic arrow', cast=0.5, coldown=0, lvl=0, finish=finish, mult=0.2)
-        self.skills.append(magic_arrow)
+        Prayer = person.classSkill.Skill(name='Prayer', cast=0.5, coldown=0, lvl=0, finish=finish, mult=0.2)
+        self.skills.append(Prayer)
 
-        '''--------------------------------------*********------------------------------------'''
-        '''--------------**********--------------Fire ball-------------**********-------------'''
-        '''--------------------------------------*********------------------------------------'''
+        '''------------------------------------------*****------------------------------------'''
+        '''--------------**********------------------Flash-------------**********-------------'''
+        '''------------------------------------------*****------------------------------------'''
 
         def finish(name: str, damage: float) -> dict:
             result = {'target': {}, 'self': []}
-            result['target']['hit'] = {
-                'name': name,
-                'type': 'magic',
-                'damage': damage,
-            }
+            result['target']['stop'] = self.stats['time']
+            return result
+
+        Flash = person.classSkill.Skill(name='Flash', cast=0.3, coldown=3, lvl=1, finish=finish, mult=0)
+        self.skills.append(Flash)
+
+        '''----------------------------------------**************-------------------------------------'''
+        '''--------------**********----------------Сleansing soul--------------**********-------------'''
+        '''----------------------------------------**************-------------------------------------'''
+
+        def finish(name: str, damage: float) -> dict:
+            result = {'target': {}, 'self': []}
+
             result['target']['dot'] = {
-                'name': 'Combustion',
+                'name': 'Сleansing soul',
                 'type': 'magic',
-                'damage': damage * 0.25,
-                'stage': 2,
-                'time': 1.4,
+                'damage': damage,
+                'stage': 4,
+                'time': 0.8,
             }
             return result
 
-        fire_ball = person.classSkill.Skill(name='Fire ball', cast=1.1, coldown=2.25, lvl=1, finish=finish, mult=0.85)
-        self.skills.append(fire_ball)
+        Сleansing_soul = person.classSkill.Skill(name='Сleansing soul', cast=0.7, coldown=4, lvl=2, finish=finish, mult=0.45)
+        self.skills.append(Сleansing_soul)
 
-        '''----------------------------------------******-------------------------------------'''
-        '''--------------**********----------------Cobble--------------**********-------------'''
-        '''----------------------------------------******-------------------------------------'''
-
+        '''---------------------------------------------*****---------------------------------------'''
+        '''--------------**********---------------------Shock----------------**********-------------'''
+        '''---------------------------------------------*****---------------------------------------'''
         def finish(name: str, damage: float) -> dict:
             result = {'target': {}, 'self': []}
             result['target']['hit'] = {
@@ -109,19 +79,58 @@ class Mag(person.Person):
                 'type': 'magic',
                 'damage': damage,
             }
-            result['target']['fall'] = {
+             result['target']['fall'] = {
                 'this_time': self.stats['time'],
-                'fall_time': 0.2,
+                'fall_time': 1.2,
             }
             return result
 
-        Cobble = person.classSkill.Skill(name='Cobble', cast=1.2, coldown=3, lvl=2, finish=finish, mult=0.7)
-        self.skills.append(Cobble)
+        Shock = person.classSkill.Skill(name='Shock', cast=1.2, coldown=5, lvl=3, finish=finish, mult=0.7)
 
-        '''---------------------------------------********------------------------------------'''
-        '''--------------**********---------------Ice ring-------------**********-------------'''
-        '''---------------------------------------********------------------------------------'''
-        cast_bonus = {'front_block': 45, 'back_block': 45}
+        self.skills.append(Shock)
+
+        '''--------------------------------------************------------------------------------'''
+        '''--------------**********--------------Сolumn light-------------**********-------------'''
+        '''--------------------------------------************------------------------------------'''
+        cast_bonus = {'evasion': 100}  
+        def finish(name: str, damage: float) -> dict:
+            result = {'target': {}, 'self': []}
+             result['self'].append({
+                'func': self.get_hitpoint,
+                'name': 'my hill',
+                'damage': damage,
+            })
+            return result
+
+        Сolumn_light = person.classSkill.Skill(name='Сolumn light', cast=1, coldown=5, lvl=4, finish=finish, mult=1)
+
+        self.skills.append(Сolumn_light)
+
+        '''--------------------------------------************-----------------------------------'''
+        '''--------------**********--------------Purification------------**********-------------'''
+        '''--------------------------------------************-----------------------------------'''
+
+        #Я ещё не сделал чистку от дибаффов в движке (а может и сделал, я уже не помню)
+        cast_bonus = {'evasion': 100}  
+        def finish(name: str, damage: float) -> dict:
+            result = {'target': {}, 'self': []}
+            result['self'].append({
+                'func': self.get_hitpoint,
+                'name': 'my hill',
+                'damage': damage,
+            })
+            return result
+
+        Purification = person.classSkill.Skill(name='Purification', cast=1, coldown=5, lvl=4, finish=finish, mult=1)
+
+        self.skills.append(Purification)
+
+        '''----------------------------------------*******--------------------------------------'''
+        '''--------------**********----------------Justice---------------**********-------------'''
+        '''----------------------------------------*******--------------------------------------'''
+
+
+        #Этот скил я тоже ещё не предусмотрел
         def finish(name: str, damage: float) -> dict:
             result = {'target': {}, 'self': []}
             result['target']['hit'] = {
@@ -129,129 +138,56 @@ class Mag(person.Person):
                 'type': 'magic',
                 'damage': damage,
             }
-            result['target']['curse'] = {
-                'name': 'frost',
-                'time': 6,
-                'speed_cast': -0.3,
-                'chance_krit': 4,
-                'evasion': 7
-            }
             return result
 
-        Ice_ring = person.classSkill.Skill(name='Ice ring', cast=1.4, coldown=4, lvl=3, finish=finish, mult=1.2, bonus=cast_bonus)
+        Justice = person.classSkill.Skill(name='Justice', cast=1.2, coldown=5, lvl=3, finish=finish, mult=1.7)
 
-        self.skills.append(Ice_ring)
+        self.skills.append(Justice)
 
-        '''--------------------------------------*********------------------------------------'''
-        '''--------------**********--------------Lightning-------------**********-------------'''
-        '''--------------------------------------*********------------------------------------'''
+        '''--------------------------------------***********----------------------------------'''
+        '''--------------**********--------------Angel wings-----------**********-------------'''
+        '''--------------------------------------***********----------------------------------'''
 
         def finish(name: str, damage: float) -> dict:
             result = {'target': {}, 'self': []}
-            result['target']['hit'] = {
-                'name': name,
-                'type': 'magic',
-                'damage': damage,
-            }
-            result['target']['curse'] = {
-                'name': 'Lightning',
-                'time': 6,
-                'resistance': 0.35,
-            }
+            result['self'].append({
+                'func': self.create_bonus,
+                'name': 'Angel wings',
+                'time': 7 * (1 + self.lvl / 10),
+                'bonus_hil': 0.01 * self.lvl,
+                'back_block': 100,
+            })
             return result
 
-        Lightning = person.classSkill.Skill(name='Lightning', cast=0.8, coldown=4, lvl=4, finish=finish, mult=1.3)
+        Angel_wings = person.classSkill.Skill(name='Angel wings', cast=0.35, coldown=9, lvl=7, finish=finish, mult=0)
+        self.skills.append(Angel_wings)
 
-        self.skills.append(Lightning)
-
-        '''----------------------------------------******-------------------------------------'''
-        '''--------------**********----------------Meteor--------------**********-------------'''
-        '''----------------------------------------******-------------------------------------'''
-
+        '''-------------------------------------***********-----------------------------------'''
+        '''--------------**********-------------Benediction------------**********-------------'''
+        '''-------------------------------------***********-----------------------------------'''
+        
         def finish(name: str, damage: float) -> dict:
             result = {'target': {}, 'self': []}
-            result['target']['hit'] = {
-                'name': name,
-                'type': 'magic',
+            result['target'].append({
+                'func': self.get_hitpoint,
+                'name': 'Benediction',
                 'damage': damage,
-            }
-            result['target']['fall'] = {
-                'this_time': self.stats['time'],
-                'fall_time': 2.2,
-            }
+            })
+            result['target'].append({
+                'func': self.create_bonus,
+                'name': 'Benediction',
+                'time': 4 * (1 + self.lvl / 10),
+                'evasion': 30 * self.lvl,
+            })
+
+            if random.randint(0, 20) < 5: result['target'][-1]['defense'] = 100 * self.lvl
+            if random.randint(0, 20) < 5: result['target'][-1]['resistance'] = 100 * self.lvl
+            if random.randint(0, 20) < 5: result['target'][-1]['damage'] = 100 * self.lvl
+            if random.randint(0, 20) < 5: result['target'][-1]['precision'] = 100 * self.lvl
             return result
 
-        Meteor = person.classSkill.Skill(name='Meteor', cast=1.7, coldown=7, lvl=5, finish=finish, mult=2.3)
-        self.skills.append(Meteor)
-
-        '''--------------------------------------*********------------------------------------'''
-        '''--------------**********--------------Ice arrow-------------**********-------------'''
-        '''--------------------------------------*********------------------------------------'''
-
-        def finish(name: str, damage: float) -> dict:
-            result = {'target': {}, 'self': []}
-            result['target']['hit'] = {
-                'name': name,
-                'type': 'magic',
-                'damage': damage,
-            }
-            result['target']['curse'] = {
-                'name': 'frost',
-                'time': 6,
-                'speed_cast': -0.3,
-                'chance_krit': 4,
-                'evasion': 7
-            }
-            return result
-
-        Ice_arrow = person.classSkill.Skill(name='Ice arrow', cast=1, coldown=3, lvl=6, finish=finish, mult=0.6)
-        self.skills.append(Ice_arrow)
-
-        '''------------------------------------**************---------------------------------'''
-        '''--------------**********------------Mult lightning----------**********-------------'''
-        '''------------------------------------**************---------------------------------'''
-
-        def finish(name: str, damage: float) -> dict:
-            result = {'target': {}, 'self': []}
-            result['target']['hit'] = {
-                'name': name,
-                'type': 'magic',
-                'damage': damage,
-            }
-            result['target']['dot'] = {
-                'name': 'Lightning',
-                'type': 'magic',
-                'damage': damage * 0.2,
-                'stage': 3,
-                'time': 1.7,
-            }
-            return result
-
-        Mult_lightning = person.classSkill.Skill(name='Mult lightning', cast=0.15, coldown=6, lvl=7, finish=finish, mult=0.1, repeat=7)
-        self.skills.append(Mult_lightning)
-
-        '''--------------------------------------*********------------------------------------'''
-        '''--------------**********--------------Fire wall-------------**********-------------'''
-        '''--------------------------------------*********------------------------------------'''
-        cast_bonus = {'front_block': 25}
-        def finish(name: str, damage: float) -> dict:
-            result = {'target': {}, 'self': []}
-            result['target']['hit'] = {
-                'name': name,
-                'type': 'magic',
-                'damage': damage,
-            }
-            result['target']['dot'] = {
-                'name': 'Combustion',
-                'type': 'magic',
-                'damage': damage * 0.25,
-                'stage': 2,
-                'time': 1.4,
-            }
-            return result
-
-        Fire_wall = person.classSkill.Skill(name='Fire wall', cast=0.9, coldown=3.5, lvl=8, finish=finish, mult=0.7, bonus=cast_bonus)
-        self.skills.append(Fire_wall)
+        Benediction = person.classSkill.Skill(name='Benediction', cast=0.9, coldown=4, lvl=8, finish=finish, mult=0.7)
+        self.skills.append(Benediction)
 
 
 
